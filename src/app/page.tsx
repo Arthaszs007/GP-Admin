5713acdb7b4ff62c7cc7cb8e997cb09f615e7651
Arthas007 <arthaszs007@gmail.com>
2024-06-12 22:12:34 -0600

    11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import LoginPage from "./login/page";
-import DashboardMain from "./pages/dashboard/dashboard-main/page";
+import GameInventory from "./pages/game/inventory/page";
 
 export default async function Home() {
   return (
     <>
       {/* <LoginPage /> */}
-      <DashboardMain />
+      <GameInventory />
     </>
   );
 }
